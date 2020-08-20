5b15e5c9fb6bf2c913d566ca729bb56cf8059dfd
Heather <dylanchoum123@yahoo.com>
2020-08-19 18:27:53 -0700

    bug: fixed css issue for footer on app.js

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import Routes from "./Routes";
 import styled from "styled-components";
 
-import { device } from "./common/MediaBreakpoints";
-
 import Header from "./common/Header";
 import Footer from "./common/Footer";
 
@@ -13,28 +11,20 @@ const Site = styled.div`
   flex-direction: column;
 `;
 
-const Wrapper = styled.div`
-  max-width: 1920;
-  margin: auto;
-  margin-top: 100px;
-  margin-left: 15%;
-  margin-right: 15%;
-
-  @media ${device.mobileM} {
-    margin-top: 50px;
-    margin-left: inherit;
-    margin-right: inherit;
-  }
+const Main = styled.div`
+  display: flex;
+  min-height: 80vh;
 `;
 
 const App = () => {
+  const pathname = window.location.pathname;
   return (
     <Site>
-      <Header />
-      <Wrapper>
+      {pathname === "/sign-in" ? "" : <Header />}
+      <Main>
         <Routes />
-      </Wrapper>
-      <Footer />
+      </Main>
+      {pathname === "/sign-in" ? "" : <Footer />}
     </Site>
   );
 };
