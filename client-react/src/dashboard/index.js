007f0c1084a4cc26d6210e7c21286bad11c90cca
Heather <dylanchoum123@yahoo.com>
2020-10-01 01:36:45 -0700

    create dashboard index for testing and added to routes

---
+++ client-react/src/dashboard/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import CategoryBox from "./CategoryBox";
+
+const Dashboard = () => {
+  return <CategoryBox title="Category 1" />;
+};
+
+export default Dashboard;
