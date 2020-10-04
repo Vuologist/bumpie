588b58e4d8f012d11a8930755fb0c5fde5383f00
Heather <dylanchoum123@yahoo.com>
2020-10-03 18:28:52 -0700

    added stuff req for show and hide category

--- client-react/src/dashboard/CategoryBox.js
+++ client-react/src/dashboard/CategoryBox.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretRight, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCaretRight,
+  faEllipsisH,
+  faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 const CategoryWrapper = styled.div`
   background-color: white;
   padding: 15px;
@@ -20,17 +24,57 @@ const Title = styled.h1`
   font-size: 25pt;
 `;
 
+const ContentWrapper = styled.div`
+  margin-left: 30px !important;
+  margin: 10px;
+  display: ${(props) => (props.display ? "block" : "none")};
+`;
+
+const IconWrapper = styled.span`
+  display: ${(props) => (props.display ? "block" : "none")};
+`;
+
 const CategoryBox = ({ title }) => {
+  const [display, setDisplay] = useState([true, false]);
+
+  const toggleCategory = (index) => {
+    setDisplay(
+      display.map((x, i) => {
+        if (index !== i) {
+          x = true;
+        } else {
+          x = false;
+        }
+        return x;
+      })
+    );
+  };
+
   return (
     <CategoryWrapper>
       <TitleWrapper>
         <div style={{ display: "flex", alignItems: "center" }}>
-          <FontAwesomeIcon icon={faCaretRight} size="lg" />
+          <IconWrapper display={display[0] ? true : false}>
+            <FontAwesomeIcon
+              icon={faCaretRight}
+              size="lg"
+              onClick={() => toggleCategory(0)}
+            />
+          </IconWrapper>
+          <IconWrapper display={display[1] ? true : false}>
+            <FontAwesomeIcon
+              icon={faCaretDown}
+              size="lg"
+              onClick={() => toggleCategory(1)}
+            />
+          </IconWrapper>
           <Title>{title}</Title>
         </div>
         <FontAwesomeIcon icon={faEllipsisH} color="#707070" />
       </TitleWrapper>
-      <div>PLACE HOLDER FOR REST OF CONTENT</div>
+      <ContentWrapper display={display[1] ? true : false}>
+        <div style={{ height: "50px" }}>PLACE HOLDER FOR REST OF CONTENT</div>
+      </ContentWrapper>
     </CategoryWrapper>
   );
 };
