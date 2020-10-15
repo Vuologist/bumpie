9cd54106019b83f7aa43e4f29edc1d57d827a667
Heather <dylanchoum123@yahoo.com>
2020-10-15 00:16:52 -0700

    "

--- client-react/src/dashboard/Dropdown.js
+++ client-react/src/dashboard/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-
+import { device } from "../common/MediaBreakpoints";
 const Card = styled.div`
   display: -webkit-box;
   display: -ms-flexbox;
