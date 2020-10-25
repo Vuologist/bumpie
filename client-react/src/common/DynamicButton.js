aa539457631037d7c73769c896dff8ad4b273e8e
Anthony Vu <tk42111@gmail.com>
2020-10-25 16:38:08 -0700

    Feat: invalid feedback on sign up and login page

--- client-react/src/common/DynamicButton.js
+++ client-react/src/common/DynamicButton.js
@@ -5,6 +5,7 @@ const Button = styled.button`
   display: inline-block;
   font-weight: 400;
   color: #212529;
+  height: 45px;
   text-align: center;
   vertical-align: middle;
   cursor: pointer;
@@ -15,10 +16,13 @@ const Button = styled.button`
   border: 1px solid transparent;
   padding: 0.375rem 0.75rem;
   font-size: 1rem;
-  line-height: 1.5;
   border-radius: 0.25rem;
   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
     border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    outline: 0;
+  }
 `;
 
 const DynamicButton = ({ text, type, onClick, className }) => {
