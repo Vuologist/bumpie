1ce3a23792b9373e83450fe6643505f0ebcdc8df
Heather <dylanchoum123@yahoo.com>
2021-04-27 20:53:52 -0700

    animation

--- client-react/src/common/Alert.js
+++ client-react/src/common/Alert.js
@@ -1,6 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
+const fadeInOut = keyframes`
+    0% {
+        opacity: 0;
+    }
+    16% {
+       opacity: 1;
+    }
+    84% {
+       opacity: 1;
+    }
+    100% {
+       opacity: 0;
+    }
+`;
 const Wrapper = styled.div`
   background-color: ${(props) => (props.bg ? "#2EC4B6" : "#E3ADAD")};
   width: 100%;
@@ -10,6 +24,7 @@ const Wrapper = styled.div`
   font-size: 25px;
   color: white;
   padding-left: 50px;
+  animation: ${fadeInOut} 3s;
 `;
 
 const Alert = ({ bg, text }) => {
